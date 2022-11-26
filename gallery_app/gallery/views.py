f55aebe43e9e8da69362a8d682002a5b52b9a14f
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-26 16:10:19 +0600

    Добавлены разрешения

--- gallery_app/gallery/views.py
+++ gallery_app/gallery/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import CreateView, ListView, UpdateView, DetailView, DeleteView
 from django.urls import reverse, reverse_lazy
-from django.http import HttpResponse
 from gallery.models import Picture
 from gallery.forms import PictureForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+
+class CustomPassesTestMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        self.object = self.get_object()
+        return self.request.user == self.object.author or self.request.user.is_superuser
 
 
 class PicturesView(ListView):
@@ -21,10 +26,11 @@ class PictureDetailView(DetailView):
     context_object_name = 'picture'
 
 
-class PictureCreateView(CreateView):
+class PictureCreateView(LoginRequiredMixin, CreateView):
     model = Picture
     form_class = PictureForm
     template_name = 'picture_create.html'
+
 
     def get_success_url(self):
         return reverse('pictures')
@@ -39,7 +45,7 @@ class PictureCreateView(CreateView):
         return redirect('pictures')
         
 
-class PictureUpdateView(UpdateView):
+class PictureUpdateView(CustomPassesTestMixin, UpdateView):
     template_name = 'picture_edit.html'
     form_class = PictureForm
     model = Picture
@@ -49,8 +55,19 @@ class PictureUpdateView(UpdateView):
         return redirect('picture_detail', pk=self.get_object.pk)
 
 
-class PictureDeleteView(DeleteView):
+class PictureDeleteView(CustomPassesTestMixin, DeleteView):
     template_name = 'picture_confirm_delete.html'
     model = Picture
     success_url = reverse_lazy('pictures')
     context_object_name = 'picture'
+        
+
+class AddToFavorite(CreateView):
+    model = Picture
+    context_object_name = 'picture'
+
+    def post(self, request, *args, **kwargs):
+        picture = get_object_or_404(Picture, pk=self.get_object.pk)
+        user = request.user.pk
+        picture.favorite.add(picture_id=picture, user_id=user)
+        return redirect('pictures')
