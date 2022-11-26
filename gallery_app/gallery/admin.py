6b25ee0f03f010906f9b30bee9a46b69808c9d81
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-26 13:34:20 +0600

    Модель Picture зарегесрирована в админ панеле

--- gallery_app/gallery/admin.py
+++ gallery_app/gallery/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from gallery.models import Picture
 
-# Register your models here.
+
+
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'picture', 'description', 'created_at', 'author')
+    list_filter = ('id', 'picture', 'description', 'created_at', 'author')
+    search_fields = ('picture', 'description', 'created_at', 'author')
+    fields = ('picture', 'description', 'created_at', 'author')
+    readonly_fields = ('id', 'created_at', 'author')
+
+
+admin.site.register(Picture, PictureAdmin)
